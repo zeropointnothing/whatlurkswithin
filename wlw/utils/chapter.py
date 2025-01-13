35a2b9cb09ea33d6a4df35ade781e9027b3d0e9b
zeropointnothing <ZeroPointNothing@outlook.com>
2025-01-13 09:31:38 -0500

    add docstring

--- wlw/utils/chapter.py
+++ wlw/utils/chapter.py
@@ -2,7 +2,17 @@ from wlw.utils.manager import Manager
 from wlw.utils.renderer import Renderer
 
 class Chapter:
+    """
+    Base class for all chapters.
+    """
     def __init__(self, manager: Manager, renderer: Renderer):
+        """
+        Ensure context is passed properly to avoid data conflicts.
+
+        Args:
+        manager (Manager): The game's current manager.
+        renderer (Renderer): The game's current renderer.
+        """
         self.title = "Unnamed!"
         self.manager = manager
         self.renderer = renderer
